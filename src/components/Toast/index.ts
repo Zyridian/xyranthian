8ff6f79883265b58297b634c7c3ff1fdf505b071
Kyle DeGuzman <kyle.deguzmanbusiness@gmail.com>
2025-04-21 23:52:59 -0700

    feat: update Toast exports

---
+++ src/components/Toast/index.ts
@@ -0,0 +1,14 @@
+import Toast from "./Toast";
+import { ToastProvider } from "./ToastProvider";
+import {
+    type ToastProps,
+    type ToastVariants,
+    type ShowToastInput,
+} from "./types";
+
+export default Toast;
+export {
+    type ToastProps,
+    type ToastVariants,
+    type ShowToastInput,
+};
